163395eb9a96ca543378d73b311e3b4182e814bb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 20:14:12 -0300

    react

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#424e74',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo:{
+    color: "#ffffff",
+    fontWeight: "bold"
+  }
+});
